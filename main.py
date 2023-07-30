bb25a58137e4628b10a7f185d5544f8da0fdcbe9
vener <venerandrei331@gmail.com>
2023-07-30 16:58:50 +0200

    Added mini screens

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import pygame
 import numpy as np;
 from models.NeuralNetwork import NeuralNetwork
 from visuals.NeuralNetworkVisualizer import NeuralNetVisualizer
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
 clock = pygame.time.Clock()
 running = True
-rect = pygame.Rect(10,10,10,10);
 
 nn = NeuralNetwork(2,3);
 nnVisualizer = NeuralNetVisualizer(nn)
@@ -17,9 +14,33 @@ for l in range(len(nn.layers)):
 nn.feedforward(np.array([[0],[0]]));
 print(nn.outputValues);
 
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+clock = pygame.time.Clock()
+running = True
+
+# Screen dimensions
+WIDTH, HEIGHT = 1600, 900
+
+# Mini-screen dimensions
+
+# Create the main screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Neural Network using Genetic Algorithms for optimization")
+
+# Create mini-screens (Surfaces)
+playgroundScreen = pygame.Surface((WIDTH//2, HEIGHT))
+infoScreen = pygame.Surface((WIDTH//2, HEIGHT//2))
+neuralNetworkScreen = pygame.Surface((WIDTH//2, HEIGHT//2))
+
+# Create font object
+font = pygame.font.Font(None, 36)  # None specifies the default font, 36 is the font size
+
+# Main game loop
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -31,21 +52,34 @@ while running:
                 if event.key == pygame.K_ESCAPE:
                      running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("red")
+    # Clear the main screen with a white color
+    screen.fill((255, 255, 255))
 
-    # RENDER YOUR GAME HERE
-    # pygame.draw.rect(screen, 'blue', rect, width=0)
-    # rect = rect.move(1,1)
-    nnVisualizer.update(screen)
-    # nnVisualizer.draw(screen)
-    # flip() the display to put your work on screen
+    # Clear mini-screens with different colors
+    playgroundScreen.fill((255, 0, 0))  # Red
+    infoScreen.fill((0, 0, 0))  # Green
+    neuralNetworkScreen.fill((0, 0, 255))  # Blue
+
+    # Render text on each mini-screen
+    playgroundText = font.render("Playground", True, (255, 255, 255))  # White text
+    infoText = font.render("Info", True, (255, 255, 255))  # White text
+    neuralNetworkText = font.render("NN Visualizer", True, (255, 255, 255))  # White text
+
+    # Blit (draw) text surfaces on the mini-screens
+    playgroundScreen.blit(playgroundText, (10, 10))
+    infoScreen.blit(infoText, (10, 10))
+    neuralNetworkScreen.blit(neuralNetworkText, (10, 10))
+
+    # Draw the neural network
+    nnVisualizer.update(neuralNetworkScreen)
+
+    # Blit (draw) mini-screens on the main scree
+    screen.blit(playgroundScreen, (0, 0))
+    screen.blit(infoScreen, (WIDTH//2, 0))
+    screen.blit(neuralNetworkScreen, (WIDTH//2, HEIGHT//2))
+
+    # Update the display
     pygame.display.flip()
-
     clock.tick(60)  # limits FPS to 60
-pygame.quit()
 
-# from models.NeuralNetwork import NeuralNetwork
-# import numpy as np
-# nn = NeuralNetwork(5,4);
-# print(nn.feedforward(np.ones((5,1))));
+pygame.quit()
